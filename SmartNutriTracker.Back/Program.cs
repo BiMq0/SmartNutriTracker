0447667c518d25cca6e4493544681c2d88e1f200
Minerva98871 <minerodi0220@gmail.com>
2025-11-26 13:03:24 -0400

    eliminacion de archivos duplicados

--- SmartNutriTracker.Back/Program.cs
+++ SmartNutriTracker.Back/Program.cs
@@ -3,16 +3,35 @@ using Microsoft.IdentityModel.Tokens;
 using SmartNutriTracker.Back.Handlers;
 using SmartNutriTracker.Back.Services.Tokens;
 using SmartNutriTracker.Back.Services.Users;
+using SmartNutriTracker.Back.Database;
+using Microsoft.EntityFrameworkCore;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<JWTSettings>(builder.Configuration.GetSection("JWT"));
+
+// DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnectionString")));
+
+// Servicios
 builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddAllScopes(); // registra servicios/mappers por convención
 
-var jwtSettings = builder.Configuration.GetSection("JWT").Get<JWTSettings>();
-var key = Encoding.UTF8.GetBytes(jwtSettings.Key!);
+// MVC, Swagger y CORS
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
+// Configuración JWT
+var jwtSettings = builder.Configuration.GetSection("JWT").Get<JWTSettings>()!;
+var key = Encoding.UTF8.GetBytes(jwtSettings.Key ?? string.Empty);
 
 builder.Services.AddAuthentication(options =>
 {
@@ -21,6 +40,8 @@ builder.Services.AddAuthentication(options =>
 })
 .AddJwtBearer(options =>
 {
+    options.RequireHttpsMetadata = false; // poner true en producción
+    options.SaveToken = true;
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
@@ -34,6 +55,15 @@ builder.Services.AddAuthentication(options =>
 });
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
 app.UseAuthentication();
 app.UseAuthorization();
