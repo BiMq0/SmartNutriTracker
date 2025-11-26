9962e8c2f895238ed4b6c2fa7652ceaf40b5b992
XimenaRandom <84157802+XimenaRandom@users.noreply.github.com>
2025-11-25 22:29:51 -0400

    Eliminacion de archivos erroneos

--- SmartNutriTracker.Back/Services/Tokens/ITokenService.cs
+++
@@ -1,8 +0,0 @@
-using SmartNutriTracker.Domain.Models.BaseModels;
-
-namespace SmartNutriTracker.Back.Services.Tokens;
-
-public interface ITokenService
-{
-    string GenerarToken(Usuario usuario);
-}
