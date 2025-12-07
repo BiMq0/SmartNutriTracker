7ce898231d449670e89c92f4739faacceba0e5ee
Riceious <johnzabaletacanoxd@gmail.com>
2025-12-06 23:23:32 -0400

    Notificaciones Alerta de Caloria

---
+++ SmartNutriTracker.Shared/Endpoints/NotificacionesEndpoints.cs
@@ -0,0 +1,8 @@
+namespace SmartNutriTracker.Shared.Endpoints
+{
+    public static class NotificacionesLimiteEndpoints
+    {
+        public const string BASE = "api/notificaciones";
+        public const string OBTENER_ALERTAS = "alertas";
+    }
+}
