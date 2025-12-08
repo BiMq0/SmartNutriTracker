7773edc0157c94b32ba21ee76c82eff56cdeb740
Bryan Iban Mamani Quispe <106822537+BiMq0@users.noreply.github.com>
2025-12-08 01:45:20 -0400

    Revert "Registro de habitos"

--- SmartNutriTracker.Shared/Endpoints/AlimentoEndpoints.cs
+++
@@ -1,8 +0,0 @@
-namespace SmartNutriTracker.Shared.Endpoints
-{
-    public static class AlimentoEndpoints
-    {
-        public const string BASE = "api/alimento/";
-        public const string OBTENER_TODOS = "ObtenerTodos";
-    }
-}
